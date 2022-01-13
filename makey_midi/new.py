84260650de833b470c43fd0e07b0b5753283c3da
Brian T. Smith <flatpick@gmail.com>
2022-01-13 04:41:39 -0600

    Add button to midi gui and basic MIDI player

--- makey_midi/new.py
+++ makey_midi/new.py
@@ -8,6 +8,7 @@
 import json
 import os
 from tkinter import *
+import pygame.midi
 
 def resolve_path(parent_dir, p):
     if not os.path.isabs(p):
@@ -54,12 +55,38 @@ class Skin:
         self.background_img = self.loadImage(self.background)
 
 
+class MidiController:
+
+    def __init__(self):
+        pygame.mixer.pre_init(44100, -16, 2, 2048)
+        pygame.midi.init()
+        self.instrument_id = 0
+        self.channel = 0
+        self.output = pygame.midi.get_default_output_id()
+        self.player = pygame.midi.Output(self.output)
+        self.player.set_instrument(self.instrument_id, self.channel)
+        self.note = None
+
+    def silence(self):
+        if self.note:
+            self.player.note_off(self.note, 127, self.channel)
+
+    def playNote(self, note):
+        self.silence()
+        self.note = note
+        self.player.note_on(note, 127, self.channel)
+
+    def terminate(self):
+        self.silence()
+
 class App:
 
     def __init__(self, skin):
         self.skin = skin
+        self.midi = MidiController()
 
     def terminate(self):
+        self.midi.terminate()
         self.root.destroy()
 
     def start(self):
@@ -69,12 +96,23 @@ class App:
         self.root.focus()
         self.root.mainloop()
 
+    def buttonHit(self, event):
+        self.midi.playNote(60)
+
     def build_ui(self):
         self.skin.resolve()
         self.root.title(self.skin.title)
         self.root.geometry('{}x{}'.format(self.skin.width, self.skin.height))
         self.background = Label(self.root, image = self.skin.background_img)
         self.background.pack()
+        bi = PhotoImage(file = 'button.png')
+        l = Label(self.root, width = 53, height = 53, bg='black', image=bi)
+        l.photo = bi
+        l.bind('<Button-1>', self.buttonHit)
+        self.root.bind('<Up>', self.buttonHit)
+        #l.grid(column=2,row=2)
+        l.place(x=300,y=200)
+
         
     
 if __name__ == '__main__':
